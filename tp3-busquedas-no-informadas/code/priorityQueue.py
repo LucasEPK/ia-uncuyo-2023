19f92727774cbde7b8c6ce6180f77944fb46bd1b
LucasEPK <lucasmoyano11@outlook.it>
2023-09-22 12:15:53 -0300

    implemented a priority queue

---
+++ tp3-busquedas-no-informadas/code/priorityQueue.py
@@ -0,0 +1,63 @@
+# This module increments a priority queue, the higher the priority the closest to the first position of the queue
+class PriorityQueueElement:
+    value = None
+    priority = None
+
+    def __init__(self, value = None, priority = None) -> None:
+        if value != None:
+            self.set_value(value)
+        if priority != None:
+            self.set_priority(priority)
+
+    # SETTERS
+    def set_value(self, value):
+        self.value = value
+
+    def set_priority(self, priority):
+        self.priority = priority
+    
+    # GETTERS
+    def get_value(self):
+        return self.value
+
+    def get_priority(self):
+        return self.priority
+    
+class PriorityQueue:
+    queue = []
+
+    def enqueue(self, element : PriorityQueueElement):
+        queue = self.get_queue()
+        elementPriority = element.get_priority()
+
+        if len(queue) == 0:
+            queue.append(element)
+        else:
+            for i in range(0, len(queue)):
+                if elementPriority >= queue[i].get_priority():
+                    queue.insert(i, element)
+                    break
+
+                if i == len(queue) - 1: # This is in case the element we are adding has the smallest priority of all the queue
+                    queue.append(element)
+    
+    def dequeue(self):
+        queue = self.get_queue()
+
+        return queue.pop(0)
+
+    def print_queue(self):
+        queue = self.get_queue()
+
+        print("[", end= "")
+        for i in range(0, len(queue)):
+            print("(", queue[i].get_value(), ",", queue[i].get_priority(), ") ; ", end="")
+        print("]")
+
+    # SETTERS
+    def set_queue(self, queue):
+        self.queue = queue
+
+    # GETTERS
+    def get_queue(self):
+        return self.queue
---
+++ tp3-busquedas-no-informadas/code/priorityTest.py
@@ -0,0 +1,26 @@
+import priorityQueue
+
+queue = priorityQueue.PriorityQueue()
+element1 = priorityQueue.PriorityQueueElement("element1", 1)
+element2 = priorityQueue.PriorityQueueElement("element2", 2)
+element3 = priorityQueue.PriorityQueueElement("element3", 3)
+element4 = priorityQueue.PriorityQueueElement("element4", 4)
+element5 = priorityQueue.PriorityQueueElement("element5", 5)
+element6 = priorityQueue.PriorityQueueElement("element6", 6)
+
+queue.enqueue(element5)
+queue.enqueue(element2)
+queue.enqueue(element1)
+queue.enqueue(element6)
+queue.enqueue(element4)
+queue.enqueue(element3)
+
+queue.print_queue()
+
+print(queue.dequeue().get_value())
+
+queue.print_queue()
+
+queue.dequeue()
+
+queue.print_queue()
