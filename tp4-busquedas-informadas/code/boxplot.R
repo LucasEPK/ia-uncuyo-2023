dde5ad695cc046acaa958622ee1f6b7aea49ec9f
LucasEPK <lucasmoyano11@outlook.it>
2023-09-24 11:45:39 -0300

    finished TP4 (added reporte)

--- tp4-busquedas-informadas/code/boxplot.R
+++ tp4-busquedas-informadas/code/boxplot.R
@@ -1,6 +1,6 @@
 library(ggplot2)
 
-no_informada_df <- readr::read_csv("https://raw.githubusercontent.com/LucasEPK/ia-uncuyo-2023/main/tp3-busquedas-no-informadas/no-informada-results.csv")
+no_informada_df <- readr::read_csv("https://raw.githubusercontent.com/LucasEPK/ia-uncuyo-2023/main/tp4-busquedas-informadas/informada-results.csv")
 
 no_informada_df
 
@@ -17,13 +17,16 @@ bfs = filter(no_informada_df, algorithm_name== "BFS")
 dfs = filter(no_informada_df, algorithm_name== "DFS")
 dls = filter(no_informada_df, algorithm_name== "DLS")
 ucs = filter(no_informada_df, algorithm_name== "UCS")
+aStar = filter(no_informada_df, algorithm_name== "A*")
 
 bfsMean = mean(bfs$explored_states)
 dfsMean = mean(dfs$explored_states)
 dlsMean = mean(dls$explored_states)
 ucsMean = mean(ucs$explored_states)
+aStarMean = mean(aStar$explored_states)
 
 bfsSD = sd(bfs$explored_states)
 dfsSD = sd(dfs$explored_states)
 dlsSD = sd(dls$explored_states)
 ucsSD = sd(ucs$explored_states)
+aStarSD = sd(aStar$explored_states)
